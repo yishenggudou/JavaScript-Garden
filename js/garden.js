5326fe484b281617e6324cc6cfa2c93992bcf5df
ZhangYiJiang <zhang-yi-jiang@hotmail.com>
2011-02-13 14:59:40 +0800

    Combined js file; added preliminary mobile support

--- js/garden.js
+++ js/garden.js
@@ -13,9 +13,8 @@ $(document).ready(function() {
         var current = $('nav ul li a.active');
         current.each(function() {
             $(this).stop().animate({
-                'backgroundColor': '#556c81',
-                'color': '#ffffff'
-
+                backgroundColor: '#556c81',
+                color: '#fff'
             }, 'slow', function() {
                 $(this).removeAttr('style');
             });
@@ -25,17 +24,19 @@ $(document).ready(function() {
         var next = $('nav ul li a[href="#' + id +'"][class!="active"]');
         if (document.location.hash !== '#' + id && !instant) {
             next.css('backgroundColor', '#556c81');
-            next.css('color', '#ffffff');
+            next.css('color', '#fff');
             next.animate({
-                'backgroundColor': '#0D2E4C',
-                'color': '#FFC76D'
-
+                backgroundColor: '#0D2E4C',
+                color: '#FFC76D'
             }, 'slow');
 
         } else {
-            next.css('backgroundColor', '#0D2E4C');
-            next.css('color', '#FFC76D');
+            next.css({
+                backgroundColor: '#0D2E4C',
+                color: '#FFC76D'
+            });
         }
+        
         next.addClass('active');
     }
 
@@ -67,6 +68,33 @@ $(document).ready(function() {
         clearTimeout(initSelection);
         highlightSection();
     });
+    
+    /*
+      Mobile, and small screen stuff
+    */
+    
+    if($.mobile) {
+        $('body').addClass('mobile');
+        
+        var $w = $(window),
+            width = $w.width();
+        
+        if(width > 1000) {
+            $w.scroll(function(){
+                
+            });
+        }
+    }
+    
     prettyPrint();
 });
 
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-20768522-1']);
+_gaq.push(['_trackPageview']);
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
